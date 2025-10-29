c2048623522e362b15005a0a734d86e7b1e7a0c3
Jeffrey Chandra <jeffreychand12@gmail.com>
2025-10-29 22:55:37 +0700

    Creating database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+      username: process.env.POSTGRES_USER || 'myuser',
+      password: process.env.POSTGRES_PASSWORD || 'mypassword',
+      database: process.env.POSTGRES_DB || 'mydb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
