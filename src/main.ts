312df781f6c0ff85150efeb39354c11272431217
Core-Coder <jouwineliepangi@gmail.com>
2025-11-07 00:22:38 +0700

    fix api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000; 
+  app.use(express.json());
+  app.useGlobalPipes(new ValidationPipe());
+  const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
     console.log (`Server is running API in MODE: ${process.env.NODE_ENV} on PORT: ${PORT}`);
   });
