d2fba56fb429eb6d52e26b49e9b1b4702e22ea8d
Jeffrey Chandra <jeffreychand12@gmail.com>
2025-11-07 16:27:23 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000; 
+  const PORT = process.env.PORT || 3001; 
   await app.listen(PORT, () => {
     console.log (`Server is running API in MODE: ${process.env.NODE_ENV} on PORT: ${PORT}`);
   });
