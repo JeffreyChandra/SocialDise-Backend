e68754c9d0fce8cef82be6b4b1e36866faaeb175
JeffreyChandra <165157688+JeffreyChandra@users.noreply.github.com>
2025-11-08 15:09:15 +0700

    Delete Proyek Backend directory

--- Proyek Backend/src/auth/jwt-auth.guard.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-// 'jwt' adalah nama default yang digunakan di JwtStrategy
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
